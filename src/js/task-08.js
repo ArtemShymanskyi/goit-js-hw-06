2173a9c6bd46a038da95edc85d9674a59ed276bd
ArtemShymanskyi <Shyma34@ukr.net>
2021-10-16 22:48:09 +0300

    начал делать задачу 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,10 @@
 
+const form = document.querySelector(".login-form");
+
+
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    
+
+  form.addEventListener("submit", handleSubmit);
