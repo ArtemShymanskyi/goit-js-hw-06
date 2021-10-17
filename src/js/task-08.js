27f6948fb0d19cc0314e2ae723119477fe068b42
ArtemShymanskyi <Shyma34@ukr.net>
2021-10-17 17:03:54 +0300

    1.8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -5,6 +5,20 @@ const form = document.querySelector(".login-form");
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    
+    if (
+      event.currentTarget.elements.email.value === '' ||
+      event.currentTarget.elements.password.value === ''
+    )
+      return alert('все поля должны быть заполнены.');
+    const dataFormResult = {};
+    const formData = new FormData(event.currentTarget);
+  
+    formData.forEach((value, name) => {
+      dataFormResult[name] = value;
+    });
+  
+    console.log(dataFormResult);
+    loginForm.reset();
+}
 
   form.addEventListener("submit", handleSubmit);
